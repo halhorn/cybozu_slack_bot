82e565505f8df60c97b1c36c769fd4aab6d03451
harumitsu.nobuta <halhorn@halmidi.com>
2018-12-18 20:26:08 +0900

    use notifier

--- bot/run.py
+++ bot/run.py
@@ -1,10 +1,9 @@
 import time
-from typing import Dict
 from bot.cybouzu_request_manager import CybouzuRequestManager
+from bot.event_notifier import EventNotifier
 
 INTERVAL_SEC = 10
 NOTIFY_BUFFER_SEC = 60 * 5
-MAX_NOTIFY_MEMORY = 1000
 
 
 def execute() -> None:
@@ -28,27 +27,6 @@ def check() -> None:
     event_list = result['events']
     event_list = [event for event in event_list if event['eventType'] != 'ALL_DAY']
     return event_list
-
-
-class EventNotifier:
-    def __init__(self):
-        self._notified_list = []
-
-    def was_notified(self, event: Dict) -> bool:
-        return event['id'] in self._notified_list
-
-    def notify(self, event: Dict) -> None:
-        print(self._format(event))
-        self._notified_list.append(event['id'])
-        if len(self._notified_list) > MAX_NOTIFY_MEMORY:
-            self._notified_list = self._notified_list[-MAX_NOTIFY_MEMORY:]
-
-    def _format(self, event: Dict) -> str:
-        return '{subject} - {facility} ({start}〜)'.format(
-            start=event['start']['dateTime'],
-            subject=event['subject'],
-            facility=event['facilities'][0]['name'] if event['facilities'] else '',
-        )
 
 
 if __name__ == '__main__':
